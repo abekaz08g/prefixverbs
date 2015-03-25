c3b471281baf02ea21a16242ce14015dcdb86680
abekaz08g <kazuya.abe@gmail.com>
2015-03-25 20:32:34 +0900

    大掛かりな更新
    まだ動きません

--- prefixverb.py
+++ prefixverb.py
@@ -2,6 +2,7 @@
 
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+import json
 
 
 def getObjectIdfromTime(gen_time):
@@ -9,172 +10,91 @@ def getObjectIdfromTime(gen_time):
     用途：
         西暦の日付からObjectIdを生成
     引数：
-        gen_time: datetimeオブジェクト。例：datetime.datetime(2010, 1, 1)
+        gen_time: 日付を表す独自アノテーション。u'2010-1-1'
 
     戻り値：
         oid: ObjectId
     """
-    oid = ObjectId.from_datetime(gen_time)
+    tl = [int(item) for item in self.gen_time.split(u'-')]
+    time = datetime.datetime(tl[0], tl[1], tl[2])
+    oid = ObjectId.from_datetime(time)
     return oid
 
 
-def connect(conf):
+def connect(dbname):
     """
     用途：
-        データベースに接続し、必要とするコレクションを返す。
+        データベースに接続する。
     引数:
-        conf: データベース接続用の設定。データベース名、コレクション名を指定。
-            形式
-            {'dbname': database name, 'colname': collection name}
+        dbname: 接続するデータベース名
     戻り値:
-        col: 接続するコレクションを返す。
+        db: 接続するデータベース。
     """
     c = MongoClient()
-    col = c[conf['dbname']][conf['colname']]
-    return col
+    db = c[dbname]
+    return db
 
 
-def setIdfield(srccol, dstcol, startid=None):
+class prefixverben:
     """
     用途：
-        データベースコレクションのスナップショットを撮る範囲を規定する。
-    引数：
-        srccol: スナップショットを撮るデータベースコレクション
-        dstcol: スナップショットを保存するデータベースコレクション
-        startid: 指定されている場合，このドキュメントidを持つドキュメンとから
-            開始
-    戻り値：
-        なし
+        接頭辞動詞を取得するためのクラス。
     """
-    endid = srccol.find({}, {u'_id': 1})[srccol.count()-1]
-    idfield = {
-        u'field': u'idfield',
-        u'startid': startid,
-        u'endid': endid
-    }
-    dstcol['head'].insert(idfield)
-
-
-class snapshot:
-    """
-    用途：
-        データベースコレクションのスナップショットを撮るためのクラス。
-    """
-    def __init__(self, srccol, dstcol, start=None):
+    def __init__(self, conf):
         """
         用途：
-            snapshotクラスのコンストラクタメソッド
+            設定ファイルconfを読み込みクラス変数をセットする。
+            prefixverbenクラスのコンストラクタメソッド
         引数：
-            srccol: スナップショットを撮るデータベースコレクション
-            dstcol: スナップショットを保存するデータベースコレクション
-            start: 指定されている場合，このドキュメントidを持つドキュメンとから
-                開始
+            conf: 設定ファイル。json形式
         戻り値：
             なし
         """
-        self.srccol = srccol
-        self.dstcol = dstcol
-        self.startid = start
-        self.endid = srccol.find({}, {u'_id': 1})[srccol.count()-1]
-        setIdfield(srccol, dstcol, start)
+        f = open(conf)
+        confd = json.load(f)
+        self.sourceDB = confd['sourceDB']
+        self.distDB = confd[u'distDB']
+        self.classification = confd[u'classification']
+        self.start = confd[u'start']
+        self.end = confd[u'end']
+        self.query = confd[u'query']
+        self.cond = confd[u'cond']
 
-    def countItems(self, q):
+    def setPVcol(self):
         """
         用途：
-            スナップショット対象のコレクションにおける、
-            クエリqで記述された要素の数を数え、
-            スナップショット保存コレクションのヘッダに保存する。
-            （field: items）
-        引数：
-            q: クエリ。[{u'label': 要素ラベル, u'pat': 要素のmongodbクエリ}]
+            接頭辞動詞コレクション(prefixverbs)の準備
+        使用するクラス変数：
+            distDB, classification
+        戻り値：なし
+        """
+        db = connect(self.distDB)
+        col = db.prefixverbs
+        f = open(self.classification)
+        pvdata = json.load(f)
+        classificationType = pvdata[u'type']
+        col.insert({u'type': classificationType})
+        for key in pvdata[u'body']:
+            bulk = []
+            for v in pvdata[u'body'][key]:
+                bulk.append({u'verb': v, u'class': key})
+            col.insert(bulk)
+        f.close()
+
+    def getData(self):
+        """
+        用途：
+            コーパスから接頭辞動詞データを取得する
+        使用するクラス変数：
+            sourceDB, start, end, query, cond
         戻り値：
-            なし
+            res: mongoDBカーソルオブジェクト
         """
-        for item in q:
-            if self.startid is not None:
-                q[u'_id'] = {u'$gt': self.startid, u'$lt': self.endid}
-            else:
-                q[u'_id'] = {u'$gt': self.startid, u'$lt': self.endid}
-            itemsdoc = {u'field': u'items'}
-            freq = self.srccol.find(q).count()
-            itemsdoc[item[u'label']] = freq
-            self.dstcol['head'].insert(itemsdoc)
-
-    def extractItem(self, q, regpat, labelledItems):
-        """
-        用途：
-            スナップショットを撮るコレクションから，特定の要素の例文idを抽出し
-            スナップショット保存コレクションのボディに保存する。
-        引数：
-            q: クエリ。[{u'tags':[タグ集合], u'pat': 要素のmongodbクエリ}]
-            regpat: 検索要素の正規表現パターン
-        """
-        clf = classifier(labelledItems)
-        if self.startid is not None:
-            q[u'_id'] = {u'$gt': self.startid, u'$lt': self.endid}
-        else:
-            q[u'_id'] = {u'$gt': self.startid, u'$lt': self.endid}
-        sentences = self.srccol.find(q[u'pat'])
-        classifySentences(clf, regpat, sentences, self.dstcol)
-
-
-def classifySentences(classifier, regpat, sentences, dstcol):
-    """
-    用途：
-        文集合を与えて，要素ごとに分類
-    引数：
-        classifier: 分類器
-        regpat: 要素の正規表現パターン
-        sentences: 文集合
-        dstcol: 保存先コレクション
-    """
-    for sentence in sentences:
-        for w in sentence[u'lemma']:
-            r = regpat.search(w)
-            if r:
-                item = r.group(0)
-                labels = classifier.classify(item)
-                dstcol[u'body'].update(
-                    {'item': item},
-                    {
-                        labels: labels,
-                        u'$addToSet': {u'ids': sentence[u'_id']}
-                    },
-                    True
-                )
-
-
-class classifier:
-    """
-    用途：
-        er-動詞を、形成規則別に分類
-    コンストラクタ引数：
-        labelledVerbs: {ラベル（A~Z）: [該当要素一覧], ...}
-    """
-    def __init__(self, labelledItems):
-        """
-        用途：
-            コンストラクタ
-        引数：
-            labelledItems:  {ラベル（A~Z）: [該当要素一覧], ...}
-        戻り値：
-            なし
-        """
-        self.classTable = labelledItems
-
-    def classify(self, item):
-        """
-        用途：
-            当該要素に対応する形成規則を決定
-        引数：
-            item: 要素
-        戻り値：
-            labels: 形成規則のラベルのリスト。該当ラベルがない場合は空のリスト。
-        """
-        labels = []
-        for label in self.classTable:
-            if item in self.classTable(label):
-                labels.append(label)
-            else:
-                pass
-        return labels
+        soid = getObjectIdfromTime(self.start)
+        eoid = getObjectIdfromTime(self.end)
+        term = {u'$gt': soid, u'$lt': eoid}
+        self.query[u'_id'] = term
+        db = connect(self.sourceDB)
+        col = db.dzeit
+        res = col.find(self.query, self.cond)
+        return res
